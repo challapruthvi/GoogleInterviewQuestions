5a6edbe42e03d799774b170777b9ff6578f6aa91
Pruthvi Kumar Reddy Challa <challapruthvi@gmail.com>
2015-04-04 21:27:50 -0400

    Completed the spiral matrix problem
    Each loop can also be printed by using the commented line in the code.

--- spiralmatrix.c
+++ spiralmatrix.c
@@ -1,2 +1,73 @@
 #include<stdio.h>
 
+int main(){
+	int m[64][64],r,c,i,j; 
+	int test_row = 0, test_col = 0;
+	
+	printf("enter the number of rows\n");
+	scanf("%d", &r);
+	printf("enter the number of columns\n");
+	scanf("%d", &c);
+	
+	int tr = r-1, tc = c-1;
+	printf("Enter the elements in the matrix\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			scanf("%d", &m[i][j]);
+		}
+	}
+	
+	printf("the given matrix is\n");
+	for(i=0;i<r;i++){
+		for(j=0;j<c;j++){
+			printf("%d", m[i][j]);
+			printf("\t");
+		}
+		printf("\n");
+	}
+	
+	while(test_row < r && test_col < c){
+		if(r == 1 && c == 1){
+			printf("The matrix consists of only one element:");
+			printf("%d\n", m[test_row][test_col]);
+			return;
+		}
+		
+		if(test_row == tr){
+			for(i=test_col;i<=tc;i++){
+				printf("%d\n", m[test_row][i]);
+				return;
+			}
+		}
+		
+		if(test_col == tc){
+			for(i=test_row;i<=tr;i++){
+				printf("%d\n", m[i][test_col]);
+				return;
+			}
+		}
+		
+		for(i=test_col;i<tc;i++){
+			printf("%d\n", m[test_row][i]);
+		}
+		
+		for(i=test_row;i<tr;i++){
+			printf("%d\n", m[i][tc]);
+		}
+		
+		for(i=tc;i>test_col;i--){
+			printf("%d\n", m[tr][i]);
+		}
+		
+		for(i=tr;i>test_row;i--){
+			printf("%d\n", m[i][test_col]);
+		}
+		
+		test_row++;
+		tr--;
+		test_col++;
+		tc--;
+	//	printf("\n"); ----------------->> For printing each and every loop inside the matrix
+		
+   }
+}
